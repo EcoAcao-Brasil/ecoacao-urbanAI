d40142ac12c2b7c1f1e732e6e77f0ca9bf7fc044
Isaque Carvalho Borges <125911178+isaquecb@users.noreply.github.com>
2025-12-31 11:15:31 -0300

    Initialize models module with ConvLSTM imports

---
+++ src/urbanai/models/__init__.py
@@ -0,0 +1,13 @@
+"""Neural Network Models"""
+
+from urbanai.models.convlstm import (
+    ConvLSTMCell,
+    ConvLSTM,
+    ConvLSTMEncoderDecoder,
+)
+
+__all__ = [
+    "ConvLSTMCell",
+    "ConvLSTM",
+    "ConvLSTMEncoderDecoder",
+]
