c31dc5f24dbe9fbc1e5e28ee058a07d2a6b0ebc6
Isaque Carvalho Borges <125911178+isaquecb@users.noreply.github.com>
2025-12-31 11:26:10 -0300

    Add UrbanAI utilities module with initial functions

---
+++ src/urbanai/utils/__init__.py
@@ -0,0 +1,27 @@
+"""UrbanAI Utilities"""
+
+from urbanai.utils.config import Config
+from urbanai.utils.logging import setup_logger
+from urbanai.utils.spatial import (
+    calculate_pixel_area,
+    extract_patch,
+    reproject_raster,
+)
+from urbanai.utils.gpu_utils import (
+    clear_gpu_memory,
+    get_device,
+    print_gpu_info,
+)
+from urbanai.utils.validation import validate_raster_files
+
+__all__ = [
+    "Config",
+    "setup_logger",
+    "reproject_raster",
+    "calculate_pixel_area",
+    "extract_patch",
+    "get_device",
+    "print_gpu_info",
+    "clear_gpu_memory",
+    "validate_raster_files",
+]
